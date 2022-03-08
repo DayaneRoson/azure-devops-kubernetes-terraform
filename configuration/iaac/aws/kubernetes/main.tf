9dcb3a6f89ffa961913f575a09347cbe28f62440
Dayane Roson <82161935+DayaneRoson@users.noreply.github.com>
2022-03-07 22:17:34 -0300

    dando um nome pro worker nodes

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -44,6 +44,7 @@ module "in28minutes-cluster" {
 
   self_managed_node_groups = [
     {
+      name = "dayvops"
       instance_type = "t2.micro"
       max_capacity  = 5
       desired_capacity = 3
