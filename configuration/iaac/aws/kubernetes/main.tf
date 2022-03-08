0f8c1270b7dd47d286f32c07ca8cd35c056cb31c
Dayane Roson <82161935+DayaneRoson@users.noreply.github.com>
2022-03-07 21:11:12 -0300

    updating subnets

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -3,6 +3,9 @@
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
 # terraform-backend-state-in28minutes-123
 # AKIA4AHVNOD7OOO6T4KI
+#arn:aws:s3:::terraform-backend-state-dayroson-123
+#AKIAQBS7NK47GV6V6NJK
+
 
 
 terraform {
@@ -33,7 +36,7 @@ module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
   cluster_version = "1.14"
-  subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
+  subnets         = ["subnet-052cb3ef3d3e80178", "subnet-0168cd474e11e5b98"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
